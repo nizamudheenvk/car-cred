b9053572fc5d8551f67f895986b2abeb94541575
muhammad nizamudheen <muhammadnizamudheen@muhammads-MacBook-Air.local>
2024-11-19 15:53:44 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const MPServer =  jsonServer.create()
+const middlewre =  jsonServer.defaults()
+const PORT = 3000
+const route = jsonServer.router('db.json')
+
+MPServer.use(middlewre)
+MPServer.use(route)
+MPServer.listen(PORT,()=>{
+    console.log(`Mpserver started at port : ${PORT} and waiting for client request!!!`);
+    
+})
